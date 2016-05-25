7616dbf83223fd103e0c8c2fbe5849cae0c011f6
bettyleung <leung@ucdavis.edu>
2016-05-25 01:10:36 -0700

    restructure, added comments, etc

--- js/loadContent.js
+++ js/loadContent.js
@@ -1,67 +1,36 @@
+/*
+* Twitch Search Stream
+* www.bettyleung.github.io/twitchSearchStream
+* 5/25/2016
+*/
+
 (function () {
    'use strict';
-
 }());
+
+//Global Vars ------------------------------------------------------------------
 var offsetNum;
 var totalResults;
 var curPageNum;
 
 
-
+//Initial Load -----------------------------------------------------------------
 function initialize(){
-
-document.getElementById("myQuery").focus();
+  document.getElementById("myQuery").focus();
 }
 
-function checkEnter(e){
-  if(e.keyCode === 13)
-    getFirstPage(e);
-}
-
-function getFirstPage(ev){
-  getTwitchData(ev);
-}
-
-function getNextPage(ev){
-    if(offsetNum < Math.floor(totalResults/10)*10){
-      curPageNum++;
-      updateOffset(10);
-      getTwitchData(ev);
-    } else if (offsetNum <= totalResults){
-      curPageNum++;
-      updateOffset(totalResults % 10);
-      getTwitchData(ev);
-    }
-}
-
-function getPrevPage(ev){
-    if(offsetNum > 10) {
-      curPageNum--;
-      updateOffset(-10);
-      getTwitchData(ev);
-
-    } else {
-      curPageNum = 1;
-      updateOffset(-updateOffset);
-    }
-
-}
-
-function updateOffset(num){
-  offsetNum += num;
-  document.getElementById('curPage').innerHTML = curPageNum;
-}
-
-
+ //JSON ------------------------------------------------------------------------
 function getTwitchData(ev){ //use JSONP here
-  var inputQuery = encodeURIComponent(document.getElementById("myQuery").value);
-  var myScript = document.createElement("script");
-  var myCallback = "&callback=handleData"
+  var inputQuery = encodeURIComponent(document.getElementById("myQuery").value),
+      myScript = document.createElement("script"),
+      myCallback = "&callback=clearOldResults",
+      url;
+
   if(ev.currentTarget.id ==="bgBtn" || ev.currentTarget.id ==="smBtn" ||ev.currentTarget.id === "myQuery"){
     myCallback = "&callback=handleMetaData";
   }
 
-  var url = "https://api.twitch.tv/kraken/search/streams?limit=10&offset=" + offsetNum + "&q=" + inputQuery + myCallback;
+  url = "https://api.twitch.tv/kraken/search/streams?limit=10&offset=" + offsetNum + "&q=" + inputQuery + myCallback;
 
   myScript.type= "application/javascript";
   myScript.src = url;
@@ -70,6 +39,9 @@ function getTwitchData(ev){ //use JSONP here
   document.getElementsByTagName('head')[0].appendChild(myScript);
 }
 
+
+
+//Rendering --------------------------------------------------------------------
 function handleMetaData(data){
   offsetNum = 10;
   totalResults = 0;
@@ -80,69 +52,130 @@ function handleMetaData(data){
   var totalPage = document.getElementById('totalPage');
   totalPage.innerHTML = (totalResults/10 > 0 ? (parseInt(totalResults/10, 10)+1) : 1);
   curPage.innerHTML = 1;
-  handleData(data);
+  clearOldResults(data);
 }
 
-function handleData(data) {
-  var resultsDiv = document.getElementById('results');
-  var removeScript = document.getElementById('jsonpScript');
-  removeScript.parentNode.removeChild(removeScript);
+//remove jsonp script and clear old results
+function clearOldResults(data){
+  var resultsDiv = document.getElementById('results'),
+    removeScript = document.getElementById('jsonpScript');
+
+  removeScript.parentNode.removeChild(removeScript);  //remove the jsonp
+
   //if results already has data then remove it before adding
   if(resultsDiv.hasChildNodes()){
     resultsDiv.innerHTML = '';
   }
 
+  renderResults(data);
+}
 
+//build the rows of result row
+function renderResults(data) {
   for(var i = 0; i < data.streams.length; i++){
-    //create a row
-    var oneResult = document.createElement('div');
+    //build a result row
+    var oneResult = document.createElement('div'),
+        imgContainer = buildimage(data, i),
+        textContainer = buildText(data, i);
+
     oneResult.setAttribute("class", "row one-result");
-    //create image
-    var imgContainer = document.createElement('div');
-    imgContainer.setAttribute("class", "six columns");
-    var picDefault = document.createElement("img");
-    picDefault.setAttribute("class","default-img");
-    var picTablet = document.createElement("img");
-    picTablet.setAttribute("class","tablet-img");
-    var picDesktop = document.createElement("img");
-    picDesktop.setAttribute("class","desktop-img");
-    picDefault.setAttribute("src", data.streams[i].preview.medium);
-    picTablet.setAttribute("src", data.streams[i].preview.template.substring(0,data.streams[i].preview.template.length-20) +"350x275.jpg");
-    picDesktop.setAttribute("src",data.streams[i].preview.large);
-
-
-    var imgLink= document.createElement("a");
-    imgLink.setAttribute("href", data.streams[i].channel.url);
-    imgLink.appendChild(picDefault);
-    imgLink.appendChild(picTablet);
-    imgLink.appendChild(picDesktop);
-    imgContainer.appendChild(imgLink)
-    //create text
-    var textContainer = document.createElement('div');
-    textContainer.setAttribute("class", "six columns");
-    var displayName = document.createElement('h4');
-    displayName.innerHTML += data.streams[i].channel.display_name;
-    var stats = document.createElement('p');
-    stats.innerHTML += "Game name: \"" + data.streams[i].game;
-    stats.innerHTML += "\" | Viewers: " + data.streams[i].viewers;
-    //create description
-    var streamDescription = document.createElement('p');
-    streamDescription.innerHTML += "This stream was created at: " + data.streams[i].created_at;
-    streamDescription.innerHTML += ". The channel status is \"" + data.streams[i].channel.status;
-    streamDescription.innerHTML += "\", and the maturity rating is " + data.streams[i].channel.mature+".";
-    textContainer.appendChild(displayName);
-    textContainer.appendChild(stats);
-    textContainer.appendChild(streamDescription);
-
-    //add everything together
     oneResult.appendChild(imgContainer);
     oneResult.appendChild(textContainer);
     resultsDiv.appendChild(oneResult);
   }
+}
 
+//build responsive images that when clicked will take to the stream
+function buildimage(data, i){
+  var imgContainer = document.createElement('div'),
+      picDefault = document.createElement("img"),
+      picTablet = document.createElement("img"),
+      picDesktop = document.createElement("img");
+      imgLink= document.createElement("a");
+
+  imgContainer.setAttribute("class", "six columns");
+  picDefault.setAttribute("class","default-img");
+  picTablet.setAttribute("class","tablet-img");
+  picDesktop.setAttribute("class","desktop-img");
+  picDefault.setAttribute("src", data.streams[i].preview.medium);
+  picTablet.setAttribute("src", data.streams[i].preview.template.substring(0,data.streams[i].preview.template.length-20) +"350x275.jpg");
+  picDesktop.setAttribute("src",data.streams[i].preview.large);
+  imgLink.setAttribute("href", data.streams[i].channel.url);
+
+  imgLink.appendChild(picDefault);
+  imgLink.appendChild(picTablet);
+  imgLink.appendChild(picDesktop);
+  imgContainer.appendChild(imgLink);
+
+  return imgContainer;
+}
+
+//build display name, game name, viewers, and description
+function buildText(){
+  var textContainer = document.createElement('div'),
+      displayName = document.createElement('h4'),
+      stats = document.createElement('p'),
+      streamDescription = document.createElement('p');
+
+  textContainer.setAttribute("class", "six columns");
+  displayName.innerHTML += data.streams[i].channel.display_name;
+  stats.innerHTML += "Game name: \"" + data.streams[i].game;
+  stats.innerHTML += "\" | Viewers: " + data.streams[i].viewers;
+  //build stream description
+  streamDescription.innerHTML += "This stream was created at: " + data.streams[i].created_at;
+  streamDescription.innerHTML += ". The channel status is \"" + data.streams[i].channel.status;
+  streamDescription.innerHTML += "\", and the maturity rating is " + data.streams[i].channel.mature+".";
+
+  textContainer.appendChild(displayName);
+  textContainer.appendChild(stats);
+  textContainer.appendChild(streamDescription);
+
+  return textContainer;
 }
 
 
+//Interactions -----------------------------------------------------------------
+//enter is pressed on input
+function checkEnter(e){
+  if(e.keyCode === 13)
+    getFirstPage(e);
+}
+//page is first loaded
+function getFirstPage(ev){
+  getTwitchData(ev);
+}
+//next button is pressed
+function getNextPage(ev){
+  if(offsetNum < Math.floor(totalResults/10)*10){
+    curPageNum++;
+    updateOffset(10);
+    getTwitchData(ev);
+  } else if (offsetNum <= totalResults){
+    curPageNum++;
+    updateOffset(totalResults % 10);
+    getTwitchData(ev);
+  }
+}
+//previous button is pressed
+function getPrevPage(ev){
+  if(offsetNum > 10) {
+    curPageNum--;
+    updateOffset(-10);
+    getTwitchData(ev);
+
+  } else {
+    curPageNum = 1;
+    updateOffset(-updateOffset);
+  }
+}
+
+
+
+//Utility Functions ------------------------------------------------------------
+function updateOffset(num){
+  offsetNum += num;
+  document.getElementById('curPage').innerHTML = curPageNum;
+}
 
 
 // function updateData(){
@@ -154,7 +187,7 @@ function handleData(data) {
 //     xhrRequest.onreadystatechange = function() {
 //       if (xhrRequest.readyState == 4 && xhrRequest.status == 200) {
 //           var myData = JSON.parse(xhrRequest.responseText);
-//           handleData(myData);
+//           renderResults(myData);
 //           totalResults = myData["_total"];
 //       }
 //   };
